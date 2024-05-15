1c9d36595138e5946f41dfc7cf785b689935de21
Neuro <118199015+NeuroException@users.noreply.github.com>
2024-05-15 20:27:48 +0200

    Initial commit

---
+++ debloat.sh
@@ -0,0 +1,36 @@
+# This script "debloats" Fedora Workstation.
+# It is pretty much a choice of taste wether or not
+# these packages are bloat or not.
+# Think before executing!
+
+dnf remove -y gnome-connections \
+	anaconda \
+	gnome-characters \
+	gnome-classic-session \
+	gnome-contacts \
+	gnome-maps \
+	gnome-photos \
+	gnome-remote-desktop \
+	gnome-system-monitor \
+	gnome-tour \
+	gnome-user-docs \
+	yelp \
+	cheese \
+	rhythmbox \
+	totem \
+	libreoffice-core \
+	gnome-boxes \
+	unoconv \
+	gnome-weather \
+	gnome-clocks \
+	gnome-calculator \
+	simple-scan \
+	mediawriter \
+	gnome-font-viewer \
+	evince \
+	gnome-disk-utility \
+	baobab \
+	gnome-font-viewer \
+	eog \
+	gnome-calendar \
+	gnome-terminal
